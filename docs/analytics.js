c6b891077ace956a9735ed467a386571ea77c6d1
NoahHsu <knight6623@gmail.com>
2023-09-30 01:21:04 +0800

    use $('head').append() to add script to head  (#5)
    * Update mkdocs.yml
    
    * Update analytics.js

--- docs/analytics.js
+++ docs/analytics.js
@@ -1,4 +1,13 @@
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-J8TC3WPHWC');
+$('head').append('
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1453160910119702"
+     crossorigin="anonymous"></script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-J8TC3WPHWC"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag("js", new Date());
+
+  gtag("config", "G-J8TC3WPHWC");
+</script>
+');
